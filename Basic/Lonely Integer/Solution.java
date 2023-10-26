6a3b5b93c60181a16ab71a643ac2a36babcd5c91
Luay Gharzeddine <lmg11@mail.aub.edu>
2023-10-26 13:14:06 +0300

    Lonely Integer Solution

---
+++ Basic/Lonely Integer/Solution.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'lonelyinteger' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int lonelyinteger(List<Integer> a) {
+    // Write your code here
+        HashMap<Integer, Integer> counts = new HashMap<>();
+        for(int num: a) {
+            int count = counts.getOrDefault(num, 0);
+            count++;
+            counts.put(num, count);
+        }
+        for(int key: counts.keySet()) {
+            int count = counts.get(key);
+            if(count == 1) return key;
+        }
+        return 0; // should never be reached because of input guarantees but throws as error if not included
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.lonelyinteger(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
